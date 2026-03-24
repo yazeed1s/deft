677389107d50309fc72ba83f54d4f9efe8b68724
Yazeed1s <yazeed4n@gmail.com>
2026-03-24 16:25:34 -0400

    update server and client counts to match 1MN + 4CN setup

--- provision/run_servers.sh
+++ provision/run_servers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # run_servers.sh - run on mn0 and mn1
 
-SERVER_COUNT=2
-CLIENT_COUNT=10
+SERVER_COUNT=1
+CLIENT_COUNT=4
 
 echo "starting deft server on $(hostname)..."
 cd /mydata/deft/build
