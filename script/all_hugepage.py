49c5199baf1ad88b5b364867548e860b79ddc498
Yazeed1s <yazeed4n@gmail.com>
2026-03-25 12:49:03 -0400

    reduce hugepage size

--- script/all_hugepage.py
+++ script/all_hugepage.py
@@ -13,13 +13,13 @@ def all_hugepage():
     for node_list in [g_cfg['clients'], g_cfg['servers']]:
         for i in range(len(node_list)):
             ip = node_list[i]['ip']
-        if ip in ip_set:
-            continue
-        ip_set.add(ip)
-        print(f'hugepage ${ip}')
-        cmd = f'sudo sysctl -w vm.nr_hugepages=65536'
-        ssh, stdin, stdout, stderr = ssh_command(ip, username, password, cmd)
-        ssh.close()
+            if ip in ip_set:
+                continue
+            ip_set.add(ip)
+            print(f'hugepage ${ip}')
+            cmd = f'sudo sysctl -w vm.nr_hugepages=32768'
+            ssh, stdin, stdout, stderr = ssh_command(ip, username, password, cmd)
+            ssh.close()
 
 if __name__ == '__main__':
     all_hugepage()
