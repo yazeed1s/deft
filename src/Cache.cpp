818a5fdf3cfd1e46daf5cc42213d1458d56e878f
Yazeed1s <yazeed4n@gmail.com>
2026-03-31 13:32:40 -0400

    batch cpp code

--- src/Cache.cpp
+++ src/Cache.cpp
@@ -1,6 +1,10 @@
 #include "Cache.h"
 
 Cache::Cache(const CacheConfig &cache_config) {
-    size = cache_config.cacheSize;
-    data = (uint64_t)hugePageAlloc(size * define::GB);
+  size = cache_config.cacheSize;
+  data = (uint64_t)hugePageAlloc(size * define::GB);
+  // Pre-fault huge pages so ibv_reg_mr does not fail on unmapped entries.
+  for (uint64_t off = 0; off < size * define::GB; off += 2 * define::MB) {
+    *(volatile char *)(data + off) = 0;
+  }
 }
